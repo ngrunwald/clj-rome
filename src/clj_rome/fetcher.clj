10f41e321275c95e19d36445cad4591b284bbf4b
Nils Grunwald <nils@grunwald.fr>
2012-02-28 15:35:33 +0100

    fixed macro, formatting and made 1.3 compatible

--- src/clj_rome/fetcher.clj
+++ src/clj_rome/fetcher.clj
@@ -1,59 +1,45 @@
 (ns clj-rome.fetcher
   (:import (com.sun.syndication.fetcher.impl HttpURLFeedFetcher)
            (com.sun.syndication.fetcher FetcherListener)
-           (java.net URL))
-  (:require [clojure.contrib.string]))
+           (java.net URL)))
 
-(def *fetcher*)
+(def ^{:dynamic true} *fetcher*)
 
 (defn build-url-fetcher
   "builds an HttpURLFeedFetcher with or without caching."
-  (
-   []
+  ([]
      (HttpURLFeedFetcher.))
-  (
-   [cache]
-     (HttpURLFeedFetcher. cache)
-     ))
+  ([cache]
+     (HttpURLFeedFetcher. cache)))
 
 (defn retrieve-feed
   "retrieves a feed from a fetcher or the default one.
    Returns a SyndFeedImpl object"
-  ( 
-   [fetcher url]
+  ([fetcher url]
      (.retrieveFeed fetcher (URL. (str url))))
-  (
-   [url]
-     (retrieve-feed *fetcher* url)
-     ))
-
+  ([url]
+     (retrieve-feed *fetcher* url)))
 
 (defn add-listener
   "adds a listener to the given fetcher object.
    Returns a FetcherListener object. Use it with
    remove-listener."
-  ( [fetcher cb]
-      (let [listener (proxy [FetcherListener] []
-                       (fetcherEvent [event] (cb event)))]
-        (.addFetcherEventListener fetcher listener)
-        listener))
-  ( [cb]
-      (add-listener *fetcher* cb)
-      ))
+  ([fetcher cb]
+     (let [listener (proxy [FetcherListener] []
+                      (fetcherEvent [event] (cb event)))]
+       (.addFetcherEventListener fetcher listener)
+       listener))
+  ([cb]
+     (add-listener *fetcher* cb)))
 
 (defn remove-listener
   "removes a listener from the given fetcher"
-  (
-   [fetcher listener]
-     (.removeFetcherEventListener fetcher listener)
-     )
-  (
-   [listener]
-     (remove-listener *fetcher* listener)
-     ))
+  ([fetcher listener]
+     (.removeFetcherEventListener fetcher listener))
+  ([listener]
+     (remove-listener *fetcher* listener)))
 
 (defmacro with-fetcher
   [fetcher & body]
-  `(binding [clj-rome.fetcher/*fetcher* fetcher]
+  `(binding [clj-rome.fetcher/*fetcher* ~fetcher]
      ~@body))
-
