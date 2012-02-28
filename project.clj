d26c66a7e78ce2ba4aa0810ec2e9d8ae08feaf67
Nils Grunwald <nils@grunwald.fr>
2012-02-28 17:05:56 +0100

    update version to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-rome "0.1.0"
+(defproject clj-rome "0.2.0-SNAPSHOT"
   :description "A Clojure wrapper for the ROME feed parsing and manipulation library"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [clj-time "0.3.7"]
