3cf2f5ead8de76db64cc36cd7fb7e4ba61e38d00
Nils Grunwald <nils@grunwald.fr>
2012-02-28 16:44:02 +0100

    bump version and release

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject clj-rome "0.0.1-SNAPSHOT"
+(defproject clj-rome "0.1.0"
   :description "A Clojure wrapper for the ROME feed parsing and manipulation library"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [clj-time "0.3.1"]
+                 [clj-time "0.3.7"]
                  [net.java.dev.rome/rome "1.0.0"]
                  [net.java.dev.rome/rome-fetcher "1.0.0"]])
